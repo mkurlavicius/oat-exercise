be75d15ed8ee513b78dc3c48a2ac79f3b6dc3a71
Mindaugas Kurlavičius <mindaugas.kurlavicius@reborn.lu>
2018-08-29 17:27:25 +0200

    Added controller and better exception handling

--- routes/api.php
+++ routes/api.php
@@ -18,29 +18,9 @@ Use App\Article;
 //    return $request->user();
 //});
 
-Route::get('articles', function() {
-    // If the Content-Type and Accept headers are set to 'application/json',
-    // this will return a JSON structure. This will be cleaned up later.
-    return Article::all();
-});
 
-Route::get('articles/{id}', function($id) {
-    return Article::find($id);
-});
-
-Route::post('articles', function(Request $request) {
-    return Article::create($request->all());
-});
-
-Route::put('articles/{id}', function(Request $request, $id) {
-    $article = Article::findOrFail($id);
-    $article->update($request->all());
-
-    return $article;
-});
-
-Route::delete('articles/{id}', function($id) {
-    Article::find($id)->delete();
-
-    return 204;
-});
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{article}', 'ArticleController@update');
+Route::delete('articles/{article}', 'ArticleController@delete');
